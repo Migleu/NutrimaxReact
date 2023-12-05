6f42d1a9c72c84c530329f7b527a0c0e46b1c205
Miguel Luz <106433901+Migleu@users.noreply.github.com>
2023-12-04 21:06:34 -0300

    Qrcode

--- Nutrimax-Teste/src/pages/Footer/Footer.jsx
+++ Nutrimax-Teste/src/pages/Footer/Footer.jsx
@@ -4,10 +4,18 @@ import iconNutriWhite from '../../assets/Icons/iconNutriWhite.svg'
 import iconFacebook from '../../assets/Icons/iconFacebook.svg';
 import iconWhatsapp from '../../assets/Icons/iconWhatsapp.svg';
 import iconInstagram from '../../assets/Icons/iconInstagram.svg';
+import QRcode from '../../assets/QrCodeNutrimax.png';
+
 
 const Footer = () => {
      return ( <>
          <div id="footer_content">
+        <div className="QRcodeContainer">
+                <h1>Leia nosso <span>Qrcode</span> para acessar a nossa <span>Documentação:</span></h1>
+            <div className="QRcode">
+                <img src={QRcode} alt="" />
+            </div>
+        </div>
              <div className="logo">
                  <img src={iconNutriWhite} alt="" />
                  <h1>Nutri<span>Max</span></h1>
@@ -30,15 +38,13 @@ const Footer = () => {
         </div>
         <div id="footer-recado">
             <p>NutriMax oferece planos personalizados, educação nutricional e apoio para ajudar você a atingir seus objetivos de saúde e bem-estar. Juntos, vamos transformar sua vida através de escolhas alimentares conscientes. Junte-se a nós nessa jornada para uma vida mais saudável e feliz.</p>
+            <h3>&copy; 2023</h3>
         </div>
 
-        
+
 
     </div>
 
-    <div id="footer_copyright">
-        <h3>&copy; 2023</h3>
-    </div>
      
      
      </> );
